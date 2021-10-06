6069d6486c4473c9c20bf3d2f70014fad45f480c
Andrija Otasevic <otasevic.andrija@gmail.com>
2021-10-06 10:28:56 -0400

    Added Share functionallity on the HomePage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  btnLabel: string = '';
+
+  constructor() {
+   }
 
   ngOnInit(): void {
+    this.btnLabel = "share" in navigator ? "Share" : "Share via e-mail";
+  }
+
+  createShareButton() {    
+  
+    const title = document.title;
+    const text = "Check this out!";
+    const url = window.location.href;
+
+  
+      if (navigator.share !== undefined) {
+        navigator
+          .share({
+            title,
+            text,
+            url
+          })
+          .then(() => console.log("Shared!"))
+          .catch(err => console.error(err));
+      } else {
+        window.location.href = `mailto:?subject=${title}&body=${text}%0A${url}`;
+      }
+
   }
 
 }
